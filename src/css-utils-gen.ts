9053bb73854dea9d1fef7d2dc7d0e68e91ba6fef
Yunier Alvarez <yunieralvarez@librem.one>
2021-12-27 00:58:12 -0500

    refactor

--- src/css-utils-gen.ts
+++ src/css-utils-gen.ts
@@ -6,15 +6,24 @@ const heightValues = range(1, 101)
   .map(i => `.h-${i}rem { height: ${i}rem !important; }`)
   .join('\r\n\r\n');
 
-const data = [['css-utils-height', heightValues]];
+const widthValues = range(1, 101)
+  .map(i => `.w-${i}rem { width: ${i}rem !important; }`)
+  .join('\r\n\r\n');
+
+const data = [
+  ['css-utils-height.css', heightValues],
+  ['css-utils-width.css', widthValues],
+];
 
 const distRoot = `${__dirname.replace('\\src', '')}\\dist`;
 
 data.forEach(([fileName, val]) => {
   const formattedValues = format(val, { parser: 'css' });
-  writeFileSync(`${distRoot}\\${fileName}.css`, formattedValues);
+  writeFileSync(`${distRoot}\\${fileName}`, formattedValues);
 });
 
-data.forEach(([fileName]) =>
-  writeFileSync(`${distRoot}\\css-utils.css`, `@import '${fileName}.css';`)
-);
+const cssImports = data
+  .map(([fineName]) => `@import '${fineName}';`)
+  .join('\r\n');
+
+writeFileSync(`${distRoot}\\css-utils.css`, cssImports);
