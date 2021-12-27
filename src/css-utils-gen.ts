dc965419969338a0824d05e24375b6039c61abb6
Yunier Alvarez <yunieralvarez@librem.one>
2021-12-27 00:40:41 -0500

    refactor

--- src/css-utils-gen.ts
+++ src/css-utils-gen.ts
@@ -2,13 +2,13 @@ import { writeFileSync } from 'fs';
 import { format } from 'prettier';
 import { range } from 'ramda';
 
-const distRoot = `${__dirname.replace('\\src', '')}\\dist`;
-
 const heightValues = range(1, 101)
   .map(i => `.h-${i}rem { height: ${i}rem !important; }`)
   .join('\r\n\r\n');
 
 const data = [['css-utils-height', heightValues]];
+
+const distRoot = `${__dirname.replace('\\src', '')}\\dist`;
 
 data.forEach(([fileName, val]) => {
   const formattedValues = format(val, { parser: 'css' });
