3fe7414c01d18b4f4533e4ced58117645a88a270
Yunier Alvarez <yunieralvarez@librem.one>
2021-12-27 00:37:13 -0500

    refactor

--- src/css-utils-gen.ts
+++ src/css-utils-gen.ts
@@ -2,41 +2,19 @@ import { writeFileSync } from 'fs';
 import { format } from 'prettier';
 import { range } from 'ramda';
 
-// const hValues = range(1, 101)
-//   .map(i => `.h-${i}rem { height: ${i}rem !important; }`)
-//   .join('\r\n\r\n');
-
-// const formattedHValues = format(hValues, { parser: 'css' });
 const distRoot = `${__dirname.replace('\\src', '')}\\dist`;
 
-const data = [
-  [
-    'css-utils-height.css',
-    (() =>
-      range(1, 101)
-        .map(i => `.h-${i}rem { height: ${i}rem !important; }`)
-        .join('\r\n\r\n'))(),
-  ],
-];
+const heightValues = range(1, 101)
+  .map(i => `.h-${i}rem { height: ${i}rem !important; }`)
+  .join('\r\n\r\n');
+
+const data = [['css-utils-height', heightValues]];
 
 data.forEach(([fileName, val]) => {
   const formattedValues = format(val, { parser: 'css' });
-  writeFileSync(`${distRoot}\\${fileName}`, formattedValues);
+  writeFileSync(`${distRoot}\\${fileName}.css`, formattedValues);
 });
 
 data.forEach(([fileName]) =>
-  writeFileSync(`${distRoot}\\css-utils.css`, `@import '${fileName}';`)
+  writeFileSync(`${distRoot}\\css-utils.css`, `@import '${fileName}.css';`)
 );
-
-/*
-.forEach(([fileName, val]) => {
-  const formattedValues = format(val, { parser: 'css' });
-  writeFileSync(`${distRoot}\\${fileName}`, formattedValues);
-});
-*/
-
-// const distRoot = `${__dirname.replace('\\src', '')}\\dist`;
-// writeFileSync(`${distRoot}\\css-utils-height.css`, formattedHValues);
-// writeFileSync(`${distRoot}\\css-utils.css`, "@import 'css-utils-height.css';");
-
-// data.forea
