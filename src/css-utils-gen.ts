af459c433b6361c5089f222d41093eb3fc2c623f
Yunier Alvarez <yunieralvarez@librem.one>
2021-12-27 01:11:17 -0500

    lazy

--- src/css-utils-gen.ts
+++ src/css-utils-gen.ts
@@ -2,22 +2,31 @@ import { writeFileSync } from 'fs';
 import { format } from 'prettier';
 import { range } from 'ramda';
 
-const heightValues = range(1, 101)
-  .map(i => `.h-${i}rem { height: ${i}rem !important; }`)
-  .join('\r\n\r\n');
+const genFontSizeValues = () =>
+  range(1, 101)
+    .map(i => `.fs-${i}rem { font-size: ${i}rem !important; }`)
+    .join('\r\n\r\n');
 
-const widthValues = range(1, 101)
-  .map(i => `.w-${i}rem { width: ${i}rem !important; }`)
-  .join('\r\n\r\n');
+const genHeightValues = () =>
+  range(1, 101)
+    .map(i => `.h-${i}rem { height: ${i}rem !important; }`)
+    .join('\r\n\r\n');
 
-const data = [
-  ['css-utils-height.css', heightValues],
-  ['css-utils-width.css', widthValues],
+const genWidthValues = () =>
+  range(1, 101)
+    .map(i => `.w-${i}rem { width: ${i}rem !important; }`)
+    .join('\r\n\r\n');
+
+const data: [string, () => string][] = [
+  ['css-utils-font-size.css', genFontSizeValues],
+  ['css-utils-height.css', genHeightValues],
+  ['css-utils-width.css', genWidthValues],
 ];
 
 const distRoot = `${__dirname.replace('\\src', '')}\\dist`;
 
-data.forEach(([fileName, val]) => {
+data.forEach(([fileName, genVal]) => {
+  const val = genVal();
   const formattedValues = format(val, { parser: 'css' });
   writeFileSync(`${distRoot}\\${fileName}`, formattedValues);
 });
