ced7ca273050494c87e3646c80662a446dc6da5e
Yunier Alvarez <yunieralvarez@librem.one>
2021-12-26 23:59:42 -0500

    gen css file

--- src/css-utils-gen.ts
+++ src/css-utils-gen.ts
@@ -1,7 +1,9 @@
+import { writeFileSync } from 'fs';
 import { range } from 'ramda';
 
-const hValues = range(1, 101).map(
-  i => `.h-${i}rem { height: ${i}rem !important; }`
-);
+const hValues = range(1, 101)
+  .map(i => `.h-${i}rem { height: ${i}rem !important; }`)
+  .join('\r\n\r\n');
 
-console.log(hValues.join('\n'));
+const rootPath = __dirname.replace('\\src', '');
+writeFileSync(`${rootPath}\\dist\\css-utils.css`, hValues);
